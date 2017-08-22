cf6b6b2778e139337c4e435b72673725eb32f33f
Jan Markowski <jan@markowski.ca>
2017-08-21 23:03:41 -0400

    Autostart the xrandr changes in .profile instead of by 'awesome'. .profile is sourced by LXDM after a user logs on

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -559,4 +559,3 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
-awful.spawn.with_shell(os.getenv("HOME") .. "/.config/awesome/autorun.sh")
--- bin/init-awesome.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-xrandr --output DVI-I-2 --output DVI-I-3 --primary --left-of DVI-I-2
