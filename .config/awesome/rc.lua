990069e20207ed1bdd33c5ba0940361ec6077cae
Jan Markowski <jan@markowski.ca>
2017-09-22 06:21:46 -0400

    Change to use awesome 'arch' theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -42,7 +42,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 -- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-beautiful.init(gears.filesystem.get_dir("config") .. "themes/blue/theme.lua")
+beautiful.init(gears.filesystem.get_dir("config") .. "themes/arch/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xfce4-terminal"
