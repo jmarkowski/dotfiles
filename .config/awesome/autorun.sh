3a6ae7e1c9b49df8b215d90ea218632c86a94f03
Jan Markowski <jan@markowski.ca>
2018-12-11 22:40:49 -0500

    Update to support new dual-monitor configuration

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 # X compositing manager to enable basic eye-candy effects
 xcompmgr
+
+# Swap the monitors
+xrandr --output DP1 --primary --rotate normal --right-of DP2 --panning 1920x1080+1080+540
+xrandr --output DP2 --rotate right --panning 1080x1920+0+0
